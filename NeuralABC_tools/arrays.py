28abf4b366dd00a5804116a4677b4dd692a5c296
Chris Steele <steelec@gmail.com>
2022-03-12 09:17:07 -0500

    corrected mean statistics and added prelim test code

--- NeuralABC_tools/arrays.py
+++ NeuralABC_tools/arrays.py
@@ -37,7 +37,36 @@ def map_vals_to_index(index_array, key_vals):
     return key_vals[index]  # fill key_vals into index
 
 
-# slower implementation for testing
+def summed_statistics_by_index(index_array, val_array, statistic='mean', ignore_zero_index=True):
+    """
+    Compute summary statistics on val_array based grouped by indices in index_array.
+    This is a basic wrapper for scipy.stats.binned_statistic.
+
+    :param index_array: np.ndarray (dtype = int) with indices defining regions over which statistic will be calculated
+    :param val_array: np.ndarray with values which will be summarized based on the index_array
+    :param statistic: one of {'mean','std','median','count','sum','min','max',function}
+    :param ignore_zero_index: skip 0 index if present {True, False}
+    :return:
+    """
+
+    if not (index_array.dtype == int):
+        print('Your index array is not of type int, please fix!')
+        return 0
+    else:
+        bins = np.unique(index_array)
+        if ignore_zero_index and (bins[0] == 0):
+            bins = bins[1:]
+        bins = np.append(bins,
+                         bins.max() + 1)  # append an additional value to the end since we specify the bin edges
+        res = binned_statistic(index_array.ravel(), val_array.ravel(), bins=bins, statistic=statistic,
+                               range=(bins.min(), bins.max()))
+        stat = res.statistic
+        # res.bin_edges
+        bin_num = res.binnumber.reshape(index_array.shape)
+        return {"stat": stat, "bin_num": res.binnumber.reshape(index_array.shape), "bin_edges":res.bin_edges}
+
+
+# slower implementations for testing
 def _loop_map_vals_to_index(index_array, key_vals):
     """
     Use for loop to map values into index array. For comparison with the faster map_vals_to_index and paranoid
@@ -55,21 +84,21 @@ def _loop_map_vals_to_index(index_array, key_vals):
     return d_out
 
 
-def summed_statistics_by_index(index_array, val_array, statistic='mean', ignore_zero_index=True):
+def _loop_mean_statistics_by_index(index_array, val_array, ignore_zero_index=True):
     """
-    Wrapper for scipy.stats.binned_statistic
-    
-    :param index_array: np.ndarray with indices defining regions over which statistic will be calculated
-    :param val_array: np.ndarray with values which will be summarized based on the index_array
-    :param statistic: one of {'mean','std','median','count','sum','min','max',function}
-    :return: 
-    """
+    Looped version of summed_statistics_by_index for testing. Only computes the mean.
 
-    if not (index_array.dtype == int):
-        print('Your index array is not of type int, please fix!')
-        return 0
-    else:
-        bins = np.unique(index_array)
-        if ignore_zero_index and (bins[0] == 0):
-            bins = bins[1:]
-        return binned_statistic(index_array, val_array, bins=bins, statistic=statistic, range=(bins.min(), bins.max()))
+    :param index_array: np.ndarray (dtype = int) with indices defining regions over which statistic will be calculated
+    :param val_array: np.ndarray with values which will be summarized based on the index_array
+    :param ignore_zero_index: skip 0 index if present {True, False}
+    :return: ndarray containing the mean statistic at each value in index_array
+    """
+    unique = np.unique(index_array)
+    if ignore_zero_index and unique[0] == 0:
+        unique = unique[1:]
+    stat = np.zeros(unique.shape)
+    for idx in unique:
+        m = (index_array == idx)
+        stat[idx] = np.mean(val_array[m])
+    return {"stat": stat}
+
