2d42c0e3c83b044584e1b8a9fa38a17c23c70ad4
andrzeng <44561881+andrzeng@users.noreply.github.com>
2022-06-28 13:11:59 -0400

    Update eigengame.py

--- NeuralABC_tools/eigengame.py
+++ NeuralABC_tools/eigengame.py
@@ -2,7 +2,22 @@ import numpy as np
 
 def calc_penalties(data, vectors, index):
     """
+    calc_penalties is a helper function used by eigengame() and should never be called outside
+    of it. 
     
+    Parameters:
+    ----------------
+    data: the data array for which we want to run PCA on
+    vectors: the collection of eigenvectors that are being calculated by eigengame()
+    index: the index of the vector within vectors that we want to calculate the penalty for
+    
+    Returns:
+    ----------------
+    The penalties array, which will be used by eigengame() to calculate the vector update
+
+    References:
+    ----------------
+    "EigenGame: PCA as a Nash Equilibrium"; Gemp et al., 2020 
     """
     vec = vectors[:, index]
     penalties = np.zeros_like(np.dot(data, vectors[:, 0]))
@@ -15,16 +30,35 @@ def calc_penalties(data, vectors, index):
 
     return penalties 
      
-def eigengame(data, n, epochs=100, learning_rate=0.1):
+def eigengame(data, n_components, epochs=100, learning_rate=0.1):
     """
-        
-    """
+    eigengame() performs PCA on input data using the "EigenGame" algorithm, developed by
+    Gemp et al. in 2020
     
+    Parameters:
+    ----------------
+    data: a Numpy array containing the data to run PCA on
+    
+    n_components: the number of principal components to extract
+    
+    epochs: the number of iterations to calculate each eigenvector. Default = 100
+    
+    learning_rate: Learning rate of the algorithm. Default = 0.1
+    
+    Returns:
+    ----------------
+    M, vectors
+    Where M is the covariance matrix and vectors is a Numpy array containing 
+    the principal components, stacked horizontally
+    
+    References:
+    ----------------
+    "EigenGame: PCA as a Nash Equilibrium"; Gemp et al., 2020 
+    """
     M = np.dot(data.T, data)
     dim = M.shape[0]
-    vectors = np.ones((dim, n))
-    for t in range(n):
-        print("Starting vector ", t)
+    vectors = np.ones((dim, n_components))
+    for t in range(n_components):
         for epoch in range(epochs):
             rewards = np.dot(data, vectors[:, t])
             penalties = calc_penalties(data, vectors, t)
